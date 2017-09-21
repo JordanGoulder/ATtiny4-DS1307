1319f2d71c6815d8cdbe94bc673c5e9eadf9c98a
Jordan Goulder <jordan.goulder@gmail.com>
2017-09-21 11:08:29 -0400

    Enable the oscillator and read the seconds

--- src/main.c
+++ src/main.c
@@ -7,13 +7,25 @@ int main(void)
 {
     i2c_init();
 
-    _delay_ms(1000);
+    // Eanble the oscillator
+    i2c_write_byte(0xD0, 1, 0);
+    i2c_write_byte(0x00, 0, 0);
+    i2c_write_byte(0x00, 0, 1);
 
+    // Setup 1Hz square wave output
     i2c_write_byte(0xD0, 1, 0);
     i2c_write_byte(0x07, 0, 0);
-    i2c_write_byte(0x90, 0, 1);
+    i2c_write_byte(0x10, 0, 1);
 
-    while (1);
+    while (1) {
+        // Read the seconds value
+        i2c_write_byte(0xD0, 1, 0);
+        i2c_write_byte(0x00, 0, 0);
+        i2c_write_byte(0xD1, 1, 0);
+        i2c_read_byte(0, 1);
+
+        _delay_ms(100);
+    }
 
     return 0;
 }
