8f146424727acf51a8750ebd03fd0bc631c8efc9
Jordan Goulder <jordan.goulder@gmail.com>
2017-09-25 18:06:31 -0400

    Toggle B0 when seconds equals 0

---
+++ src/bcd.h
@@ -0,0 +1,6 @@
+#ifndef BCD_H
+#define BCD_H
+
+uint8_t bcd_to_bin(uint8_t bcd);
+
+#endif // BCD_H
--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
+#include <avr/io.h>
 #include <util/delay.h>
 #include <stdint.h>
 
 #include "i2c.h"
+#include "bcd.h"
 
 int main(void)
 {
+    DDRB |= _BV(DDB0);
+    PORTB |= _BV(DDB0);
+
     i2c_init();
 
     // Eanble the oscillator
@@ -28,8 +33,16 @@ int main(void)
         i2c_write(0x00);
         i2c_start();
         i2c_write(0xD1);
-        i2c_read(1);
+        uint8_t bcd_secs = 0x3F & i2c_read(1);
         i2c_stop();
+
+        uint8_t secs = bcd_to_bin(bcd_secs);
+
+        if (secs == 0) {
+            PORTB |= _BV(DDB0);
+        } else {
+            PORTB &= ~_BV(DDB0);
+        }
 
         _delay_ms(100);
     }
